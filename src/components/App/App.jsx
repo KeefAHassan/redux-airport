373fd9365d8415b908deb04706ad49d959c84fb2
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-21 11:08:51 -0600

    completed the assignment

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-
+  const airportList = useSelector((store) => store.airportList);
+  console.log(airportList);
+  const dispatch = useDispatch();
+  const [plane, setPlane] = useState("");
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <input
+        placeholder="Airline Name"
+        value={plane}
+        onChange={(e) => setPlane(e.target.value)}
+      />
+      <button onClick={() => dispatch({ type: "ADD_PLANE", payload: plane })}>
+        Add Airline
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {airportList.map((item) => (
+            <tr>
+              <td> {item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import logger from "redux-logger";
 /** TODO: import REDUX **/
-
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 /** TODO: Add REDUCERS */
+const airportList = (state = [], action) => {
+  // console.log(`Hey, I'm the count reducer!!!!`, action);
 
+  // Since the store passes all actions to all reducers,
+  // having a system for naming your actions is a good idea.
+  if (action.type === "ADD_PLANE") {
+    // console.log('doing the increase!');
+    // When we return something, that value becomes the value of our "count" variable.
+    return [...state, action.payload];
+  }
+
+  return state;
+};
 
 /** TODO: Create store */
 
-
+const storeInstance = createStore(
+  combineReducers({
+    airportList,
+  }),
+  // Tell Redux that we want to use our logger.
+  // apply middlewear: hey, this thing... let it snoop.
+  // In this case, the thing we let snoop is our logger.
+  applyMiddleware(logger)
+);
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
